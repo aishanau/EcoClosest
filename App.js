1b2010d3fe048d5bb0fcb3d12fdebf4bbadb0db7
rovielynesp <rovielynespiritu@outlook.com>
2022-11-14 16:24:03 +1100

    search bar done

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+
 import ItemCard from './components/ItemCard';
 import ButtonIcon from './components/ButtonIcon';
+import SearchBar from './components/SearchBar';
+
 import titles from './index.js';
 
 export default function App() {
   return (
     <View style={styles.container}>
       {/* <ItemCard /> */}
-      <ButtonIcon name={titles.shirt}/>
+      {/* <ButtonIcon name={titles.shirt}/> */}
+      <SearchBar />
     </View>
   );
 }
---
+++ components/SearchBar.js
@@ -0,0 +1,41 @@
+import * as React from "react";
+import { SearchBar } from "@rneui/base";
+import { View, StyleSheet } from 'react-native';
+
+export default () => {
+  const [value, setValue] = React.useState("");
+  return (
+    <View style={styles.container}>
+        <SearchBar
+        platform="ios"
+        containerStyle={styles.inner}
+        inputContainerStyle={styles.inner}
+        inputStyle={{fontColor: 'white'}}
+        searchIcon={{color: '#fff'}}
+        loadingProps={{}}
+        onChangeText={newVal => setValue(newVal)}
+        onClearText={() => console.log(onClearText())}
+        placeholder="Search items"
+        placeholderTextColor="#fff"
+        round
+        cancelButtonTitle="cancel"
+        cancelButtonProps={{color: '#F9F7F7', margin: 5}}
+        onCancel={() => console.log('cancelled')}
+        value={value}
+        />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        width: 300,
+        borderRadius: 20,
+    },
+    inner: {
+        backgroundColor: '#FFD0D0',
+        borderRadius: 30,
+    }
+    });
+
+
