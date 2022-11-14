37387a0a51aa8aa1002ef7b476a6ad46a578633d
rovielynesp <rovielynespiritu@outlook.com>
2022-11-14 18:15:15 +1100

    scroll being weird

--- pages/BrowsePage.js
+++ pages/BrowsePage.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Saf } from 'react-native';
 
 import ItemCard from '../components/ItemCard';
 import ButtonIcon from '../components/ButtonIcon';
 import SearchBar from '../components/SearchBar';
+
+import { Button, ButtonGroup, withTheme } from '@rneui/themed';
 
 import titles from '../index.js';
 
@@ -17,6 +19,7 @@ export default function BrowsePage() {
     ]);
 
   return (
+    <ScrollView style={styles.scrollView}>
     <View style={styles.container}>
         <Text style={styles.header}>
             Shop Sustainably
@@ -34,6 +37,35 @@ export default function BrowsePage() {
                 key={idx}
                 title={title}
                 image={image}
+                />
+      ))}
+
+        </View>
+        <Button
+              title="Browse all categories"
+              loading={false}
+              loadingProps={{ size: 'small', color: 'white' }}
+              buttonStyle={styles.btn}
+              titleStyle={{ fontWeight: '600', fontSize: 16, color: '#FB5C5C' }}
+              containerStyle={{
+                marginHorizontal: 50,
+                height: 50,
+                width: 200,
+                marginVertical: 10,
+              }}
+              onPress={() => console.log('aye')}
+            />
+
+        <Text style={styles.title}>
+            On Sale
+        </Text>
+        {/* <View style={styles.album2}>
+            <ScrollView
+            horizontal={true}
+            >
+            {category.map(({ title, image }, idx) => (
+                <ItemCard
+                key={idx}
                 // onPress={() =>
                 //     navigation.navigate("Details", {
                 //     title,
@@ -43,12 +75,13 @@ export default function BrowsePage() {
                 // }
                 />
       ))}
-
-        </View>
-
+            </ScrollView>
+        </View> */}
       {/* <ItemCard /> */}
       {/* <ButtonIcon name={titles.shirt}/> */}
     </View>
+    </ScrollView>
+
   );
 }
 
@@ -66,6 +99,14 @@ const styles = StyleSheet.create({
     flexWrap: 'wrap',
     justifyContent: 'space-evenly',
     alignContent: 'space-evenly',
+    marginBottom: 10
+  },
+  album2: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignContent: 'space-evenly',
+    marginBottom: 10
   },
   header: {
     margin: 5,
@@ -75,5 +116,13 @@ const styles = StyleSheet.create({
     fontColor: '#FB5C5C',
     fontSize: '20px',
     fontWeight: '600'
-  }
+  },
+  btn: {
+    backgroundColor: '#FFD0D0',
+    borderRadius: 30,
+  },
+  scrollView: {
+    backgroundColor: 'pink',
+    marginHorizontal: 6
+  },
 });
