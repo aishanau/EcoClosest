b22024929adc85a3c5e884e1e8cf480babbf7866
rovielynesp <rovielynespiritu@outlook.com>
2022-11-14 13:43:32 +1100

    added card

--- components/ItemCard.js
+++ components/ItemCard.js
@@ -1,20 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Card } from '@rneui/themed';
 
 export default function ItemCard() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <Card containerStyle={styles.card}>
+          <Card.Image
+            style={{ padding: 0 }}
+            source={{
+              uri:
+                'https://awildgeographer.files.wordpress.com/2015/02/john_muir_glacier.jpg',
+            }}
+          />
+        <View style={styles.innerCard}>
+          <Text style={styles.title}>
+            Outfit Name
+          </Text>
+          <Text style={styles.price}>
+            $Price 
+          </Text>
+        </View>
+        </Card>
     </View>
   );
 }
 
+// Issue: font color not changing
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
+  },
+  card: {
+    border: '10px',
+    borderColor: '#FFD0D0',
+    borderRadius: 20,
+  },
+  innerCard: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  title: { 
+    margin: 10, 
+    fontColor: '#565454', 
+    fontSize: '18px',
+  },
+  price: { 
+    fontSize: '16px',
+    fontColor: '#D8143A'
   },
 });
