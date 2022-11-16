868786ec2c58d559a4799a924246080e21a6e4d4
Aisha Nauman <aishanauman01@gmail.com>
2022-11-16 21:51:59 +1100

    done with navbar

--- App.js
+++ App.js
@@ -1,20 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StyleSheet, Text, SafeViewArea } from "react-native";
+import SamplePage from "./components/SamplePage";
+import SamplePage2 from "./components/SamplePage2";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import { PRIMARY_COLOUR } from "./styles";
+
+const RootStack = createNativeStackNavigator();
+const Tabs = createBottomTabNavigator();
 
 export default function App() {
+  
+  const TabsNav = () => (
+    <Tabs.Navigator screenOptions={{ headerShown: false }}>
+      <Tabs.Screen
+        name="Wardrobe"
+        component={SamplePage}
+        options={{
+          tabBarIcon: ({ size }) => <MaterialCommunityIcons name="hanger" size={size} color={PRIMARY_COLOUR} />,
+        }}
+      />
+      <Tabs.Screen
+        name="Shop"
+        component={SamplePage2}
+        options={{
+          tabBarIcon: ({ size }) => <Ionicons name="md-search" size={size} color={PRIMARY_COLOUR} />,
+        }}
+      />
+      <Tabs.Screen
+        name="Cart"
+        component={SamplePage}
+        options={{
+          tabBarIcon: ({ size }) => <Ionicons name="md-cart" size={size} color={PRIMARY_COLOUR} />,
+        }}
+      />
+      <Tabs.Screen
+        name="Account"
+        component={SamplePage2}
+        options={{
+          tabBarIcon: ({ size }) => <MaterialCommunityIcons name="account" size={size} color={PRIMARY_COLOUR} />,
+        }}
+      />
+    </Tabs.Navigator>
+  );
+
+  const tabBarOptions = {
+    indicatorStyle: { colour: PRIMARY_COLOUR, },
+    activeTintColor: PRIMARY_COLOUR,
+    pressOpacity: 1,
+    style: { colour: PRIMARY_COLOUR, },
+
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <RootStack.Navigator tabBarOptions={tabBarOptions} initialRouteName="EcoCloset">
+        <RootStack.Screen name="Wardrobe" component={SamplePage} />
+        <RootStack.Screen name="Shop" component={SamplePage2} />
+        <RootStack.Screen name="Cart" component={SamplePage} />
+        <RootStack.Screen name="Account" component={SamplePage2} />
+        <RootStack.Screen name="EcoCloset" component={TabsNav} />
+      </RootStack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
